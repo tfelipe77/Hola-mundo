744647d1270241557602a70ae46ede8ef2c81ec2
unknown <soporte@calima-3.ugca.local>
2025-06-20 09:17:11 -0500

    este es mi segundo commit

---
+++ Taller.py
@@ -0,0 +1,9 @@
+
+num1 = int(input("Ingrese el primer numero entero: "))
+num2 = int(input("Ingrese el segundo numero entero: "))
+
+print(f"/nRseultados de las operaciones basicas con {num1} y {num2}: ")
+print(f"suma: {num1 + num2}")
+print(f"Resta: {num1 - num2}")
+print(f"multiplicacion: {num1 * num2}")
+print(f"Division: {num1/num2}")
